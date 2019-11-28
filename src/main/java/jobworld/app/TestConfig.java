4dc2c480a00baffc64a81cabce0cf4eec8b64e61
Lorenzo Giuliani <lory_extr3me@hotmail.it>
2019-11-28 09:12:25 +0100

    aggiunto config

---
+++ src/main/java/jobworld/app/TestConfig.java
@@ -0,0 +1,28 @@
+package jobworld.app;
+/**
+ * Classe DataServiceConfig per far partire la Webapp
+ * 
+ * @author Giuseppe Costantini
+ * @author Simone di Saverio
+ * @author Lorenzo Giuliani
+ * @author Savio Feng
+ * @version 1.0
+ */
+import java.util.Properties;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = {"jobworld.model","jobworld.services"})
+@EnableTransactionManagement
+public class TestConfig extends DataServiceConfig {
+
+	protected Properties hibernateProperties() {
+		Properties hibernateProp = super.hibernateProperties();
+		hibernateProp.put("javax.persistence.schema-generation.database.action", "drop-and-create");
+		return hibernateProp;
+	}
+
+}
