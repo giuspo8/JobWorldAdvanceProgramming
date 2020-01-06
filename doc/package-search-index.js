40fe498913730eb9af12dddfd2fb77f98f3b4565
ssa123 <xiaoli.savio@gmail.com>
2020-01-06 13:05:18 +0100

    il resto del java doc che non aveva caricato

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jobworld.app"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jobworld.app"},{"l":"jobworld.controller"},{"l":"jobworld.model.dao"},{"l":"jobworld.model.entities"},{"l":"jobworld.security"},{"l":"jobworld.services"},{"l":"jobworld.test"},{"l":"jobworld.utils"},{"l":"test.JobWorld"}]
